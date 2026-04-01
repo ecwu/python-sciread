a362c6c9e5f5405c47d8bc768323dca550b98fe9
ecwu <bitlabwzh@gmail.com>
2026-04-01 23:37:22 +0100

    refactor: remove unused compute import from use cases module

--- src/sciread/application/use_cases/__init__.py
+++ src/sciread/application/use_cases/__init__.py
@@ -2,12 +2,10 @@
 
 from .coordinate import run_coordinate_analysis
 from .discussion import run_discussion_analysis
-from .misc import compute
 from .react import run_react_analysis
 from .simple import run_simple_analysis
 
 __all__ = [
-    "compute",
     "run_coordinate_analysis",
     "run_discussion_analysis",
     "run_react_analysis",
